f00ef475804e9c123e095a63dc787dbecf84f2df
boma25 <amakirisamuelboma@gmail.com>
2020-12-12 10:07:42 +0100

    uupdate

--- musicRecomender/views.py
+++ musicRecomender/views.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
-import requests
+#import requests
 from .models import *
 
 
